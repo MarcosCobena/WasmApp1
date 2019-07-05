3a7b2ae914dc76e3bdc93ddcf468030494b0581f
Marcos Cobeña Morián <marcoscm.digital@gmail.com>
2019-07-05 19:17:19 +0200

    Initial commit :-)

---
+++ WasmApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using WebAssembly;
+
+namespace WasmApp1
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            {
+                using (var document = (JSObject)Runtime.GetGlobalObject("document"))
+                using (var body = (JSObject)document.GetObjectProperty("body"))
+                using (var button = (JSObject)document.Invoke("createElement", "button"))
+                {
+                    button.SetObjectProperty("innerHTML", "Click me!");
+                    button.SetObjectProperty(
+                        "onclick", 
+                        new Action<JSObject>(_ => 
+                        {
+                            using (var window = (JSObject)Runtime.GetGlobalObject())
+                            {
+                                window.Invoke("alert", "Hello, Wasm!");
+                            }
+                        }));
+                    body.Invoke("appendChild", button);
+                }
+            }
+        }
+    }
+}
