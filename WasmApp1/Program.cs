7fb00d3185576826f26817fa01a7f572c08cc5d2
Marcos Cobeña Morián <marcoscm.digital@gmail.com>
2019-07-16 13:34:49 +0200

    Reproduced missing System.CodeDom.dll bug

--- WasmApp1/Program.cs
+++ WasmApp1/Program.cs
@@ -7,24 +7,24 @@ namespace WasmApp1
     {
         private static void Main()
         {
-            {
-                using (var document = (JSObject)Runtime.GetGlobalObject("document"))
-                using (var body = (JSObject)document.GetObjectProperty("body"))
-                using (var button = (JSObject)document.Invoke("createElement", "button"))
-                {
-                    button.SetObjectProperty("innerHTML", "Click me!");
-                    button.SetObjectProperty(
-                        "onclick", 
-                        new Action<JSObject>(_ => 
-                        {
-                            using (var window = (JSObject)Runtime.GetGlobalObject())
-                            {
-                                window.Invoke("alert", "Hello, Wasm!");
-                            }
-                        }));
-                    body.Invoke("appendChild", button);
-                }
-            }
+            var foo = System.CodeDom.Compiler.CodeDomProvider.CreateProvider("CSharp");
+
+            //using (var document = (JSObject)Runtime.GetGlobalObject("document"))
+            //using (var body = (JSObject)document.GetObjectProperty("body"))
+            //using (var button = (JSObject)document.Invoke("createElement", "button"))
+            //{
+            //    button.SetObjectProperty("innerHTML", "Click me!");
+            //    button.SetObjectProperty(
+            //        "onclick", 
+            //        new Action<JSObject>(_ => 
+            //        {
+            //            using (var window = (JSObject)Runtime.GetGlobalObject())
+            //            {
+            //                window.Invoke("alert", "Hello, Wasm!");
+            //            }
+            //        }));
+            //    body.Invoke("appendChild", button);
+            //}
         }
     }
 }
