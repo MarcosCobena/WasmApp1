1075fbc37faa7f1abacce5e73863fde65c54f0ee
Marcos Cobeña Morián <mcobena@plainconcepts.com>
2019-10-22 16:52:21 +0200

    Reproduced Trace.TraceError() bug

--- WasmApp1/Program.cs
+++ WasmApp1/Program.cs
@@ -7,22 +7,7 @@ namespace WasmApp1
     {
         private static void Main()
         {
-            using (var document = (JSObject)Runtime.GetGlobalObject("document"))
-            using (var body = (JSObject)document.GetObjectProperty("body"))
-            using (var button = (JSObject)document.Invoke("createElement", "button"))
-            {
-                button.SetObjectProperty("innerHTML", "Click me!");
-                button.SetObjectProperty(
-                    "onclick", 
-                    new Action<JSObject>(_ => 
-                    {
-                        using (var window = (JSObject)Runtime.GetGlobalObject())
-                        {
-                            window.Invoke("alert", "Hello, Wasm!");
-                        }
-                    }));
-                body.Invoke("appendChild", button);
-            }
+            System.Diagnostics.Trace.TraceError("This should have crashed");
         }
     }
 }
