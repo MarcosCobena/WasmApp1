9ddc15897500afeebdecd6fb2bcaf2eed9b300fe
Marcos Cobeña Morián <mcobena@plainconcepts.com>
2019-10-22 16:38:40 +0200

    Removed useless braces

--- WasmApp1/Program.cs
+++ WasmApp1/Program.cs
@@ -7,23 +7,21 @@ namespace WasmApp1
     {
         private static void Main()
         {
+            using (var document = (JSObject)Runtime.GetGlobalObject("document"))
+            using (var body = (JSObject)document.GetObjectProperty("body"))
+            using (var button = (JSObject)document.Invoke("createElement", "button"))
             {
-                using (var document = (JSObject)Runtime.GetGlobalObject("document"))
-                using (var body = (JSObject)document.GetObjectProperty("body"))
-                using (var button = (JSObject)document.Invoke("createElement", "button"))
-                {
-                    button.SetObjectProperty("innerHTML", "Click me!");
-                    button.SetObjectProperty(
-                        "onclick", 
-                        new Action<JSObject>(_ => 
+                button.SetObjectProperty("innerHTML", "Click me!");
+                button.SetObjectProperty(
+                    "onclick", 
+                    new Action<JSObject>(_ => 
+                    {
+                        using (var window = (JSObject)Runtime.GetGlobalObject())
                         {
-                            using (var window = (JSObject)Runtime.GetGlobalObject())
-                            {
-                                window.Invoke("alert", "Hello, Wasm!");
-                            }
-                        }));
-                    body.Invoke("appendChild", button);
-                }
+                            window.Invoke("alert", "Hello, Wasm!");
+                        }
+                    }));
+                body.Invoke("appendChild", button);
             }
         }
     }
