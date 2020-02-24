a33c84e0c5491b9c573650294b2aa09766556ee1
Marcos Cobeña Morián <mcobena@plainconcepts.com>
2020-02-24 15:33:23 +0100

    Added demo to isolate ArrayPool.Shared.Rent() calls

--- WasmApp1/Program.cs
+++ WasmApp1/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Buffers;
+using System.Diagnostics;
 using WebAssembly;
+using WebAssembly.Core;
 
 namespace WasmApp1
 {
@@ -7,19 +10,28 @@ namespace WasmApp1
     {
         private static void Main()
         {
+            Stopwatch stopWatch = new Stopwatch();
+
             using (var document = (JSObject)Runtime.GetGlobalObject("document"))
             using (var body = (JSObject)document.GetObjectProperty("body"))
             using (var button = (JSObject)document.Invoke("createElement", "button"))
             {
-                button.SetObjectProperty("innerHTML", "Click me!");
+                button.SetObjectProperty("innerHTML", "Rent array");
                 button.SetObjectProperty(
                     "onclick", 
                     new Action<JSObject>(_ => 
                     {
-                        using (var window = (JSObject)Runtime.GetGlobalObject())
-                        {
-                            window.Invoke("alert", "Hello, Wasm!");
-                        }
+                        const int imageSide = 8 * 1024;
+                        
+                        stopWatch.Restart();
+
+                        var pixelsComingFromCanvasKit = new Uint8Array(imageSide * imageSide * 4);
+                        var array = ArrayPool<byte>.Shared.Rent(pixelsComingFromCanvasKit.Length);
+                        pixelsComingFromCanvasKit.CopyTo(array);
+
+                        stopWatch.Stop();
+
+                        Trace.WriteLine($"{stopWatch.ElapsedMilliseconds} ms");
                     }));
                 body.Invoke("appendChild", button);
             }
