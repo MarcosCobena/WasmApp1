18cc5acab7216fcfd24a4f04b871408081b0e188
Marcos Cobeña Morián <mcobena@plainconcepts.com>
2020-05-21 12:43:02 +0200

    Added WebGL.NET performance tests

--- WasmApp1/Program.cs
+++ WasmApp1/Program.cs
@@ -1,28 +1,64 @@
 ﻿using System;
+using System.Diagnostics;
 using WebAssembly;
+using WebGLDotNET;
 
 namespace WasmApp1
 {
     internal class Program
     {
+        private static JSObject window;
+        private static WebGL2RenderingContext gl;
+        private static Action<double> loopAction = new Action<double>(Loop);
+
+        private static Stopwatch stopwatch = new Stopwatch();
+        private static int iteration_counter = 0;
+        private static int iteration_counter_global = 0;
+        private static double do_frame_elapsed_ms = 0;
+        private static double do_frame_elapsed_ms_global = 0;
+        private static double iteration_elapsed_ms;
+        private static double iteration_elapsed_ms_global = 0;
+        private static double previousTimestampMilliseconds = 0;
+
         private static void Main()
         {
-            using (var document = (JSObject)Runtime.GetGlobalObject("document"))
-            using (var body = (JSObject)document.GetObjectProperty("body"))
-            using (var button = (JSObject)document.Invoke("createElement", "button"))
+            window = (JSObject)Runtime.GetGlobalObject();
+            var document = (JSObject)window.GetObjectProperty("document");
+            var canvas = (JSObject)document.Invoke("getElementById", "canvas");
+            var contextAttributes = new WebGLContextAttributes { Antialias = false };
+            gl = new WebGL2RenderingContext(canvas, contextAttributes);
+            gl.ClearColor(1.0f, 0.0f, 0.0f, 0.0f);
+            Loop(0);
+        }
+
+        private static void Loop(double timestampMilliseconds)
+        {
+            stopwatch.Restart();
+            gl.Clear(WebGLRenderingContextBase.COLOR_BUFFER_BIT);
+            stopwatch.Stop();
+
+            iteration_counter++;
+            iteration_elapsed_ms += timestampMilliseconds - previousTimestampMilliseconds;
+            previousTimestampMilliseconds = timestampMilliseconds;
+            do_frame_elapsed_ms += stopwatch.Elapsed.TotalMilliseconds;
+            
+            if (iteration_counter >= 6000)
             {
-                button.SetObjectProperty("innerHTML", "Click me!");
-                button.SetObjectProperty(
-                    "onclick", 
-                    new Action<JSObject>(_ => 
-                    {
-                        using (var window = (JSObject)Runtime.GetGlobalObject())
-                        {
-                            window.Invoke("alert", "Hello, Wasm!");
-                        }
-                    }));
-                body.Invoke("appendChild", button);
+                iteration_counter_global += iteration_counter;
+                do_frame_elapsed_ms_global += do_frame_elapsed_ms;
+                iteration_elapsed_ms_global += iteration_elapsed_ms;
+                Console.WriteLine(
+                    $"glClear: {do_frame_elapsed_ms / iteration_counter} ms, " +
+                    $"mean: {do_frame_elapsed_ms_global / iteration_counter_global} ms");
+                Console.WriteLine(
+                    $"FPS: {(iteration_counter / iteration_elapsed_ms) * 1000}, " +
+                    $"mean: {(iteration_counter_global / iteration_elapsed_ms_global) * 1000}");
+                iteration_counter = 0;
+                iteration_elapsed_ms = 0;
+                do_frame_elapsed_ms = 0;
             }
+
+            window.Invoke("requestAnimationFrame", loopAction);
         }
     }
 }
